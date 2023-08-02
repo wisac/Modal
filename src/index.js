7a2e99f1899b26fd10fea051c4cf9d12bb29efc5
Isaac Wilson <iswilson.dev@gmail.com>
2023-08-02 08:45:42 +0000

    initial commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const showModalBtns = document.querySelectorAll(".show-modal");
+const closeModalBtn = document.querySelector(".close-modal");
+const overlay = document.querySelector(".overlay");
+const hidden = document.querySelector(".hidden");
+const modal = document.querySelector(".modal");
+
+//displaying each modal by making each 'not hidden' when they're clicked
+for (let btn of showModalBtns) {
+    btn.addEventListener("click", function () {
+        modal.classList.remove("hidden");
+        overlay.classList.remove("hidden");
+    });
+}
+
+// A function to addding classes to hide element
+const closeModal = () => {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+}
+
+// when close button or overlay is clicked on
+closeModalBtn.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+//alisten and handle keydown event on the entire document
+document.addEventListener("keydown", function (event) {
+    console.log(event)
+});
